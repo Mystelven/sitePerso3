87ca0661d35b79b06aa10a38f5b53369698c336f
Mystelven <valentin.montmirail@gmail.com>
2015-06-12 08:44:50 +0200

    problème de base de données

--- src/main/java/com/polytech/mystelven/model/MysqlConnector.java
+++ src/main/java/com/polytech/mystelven/model/MysqlConnector.java
@@ -24,7 +24,7 @@ public class MysqlConnector implements Serializable {
     /** The password for the Database.  */
     private static String password = null;
 
-    private static boolean localhost = true;
+    private static boolean localhost = false;
 
     /** The URL of the MySQL database that we will access by JDBC. */
     static {
