7a8df3728d48dd1a5d18a465d9d2aefc7568e28b
Mystelven <valentin.montmirail@gmail.com>
2015-06-21 23:51:53 +0200

    optimisation chargement page

--- src/main/java/com/polytech/mystelven/model/MysqlConnector.java
+++ src/main/java/com/polytech/mystelven/model/MysqlConnector.java
@@ -27,7 +27,7 @@ public class MysqlConnector implements Serializable {
 
     private static Connection cn = null;
 
-    
+
     private static boolean localhost = false;
 
     /** The URL of the MySQL database that we will access by JDBC. */
