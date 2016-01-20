efde91c5e96d71155d73a53d0c57a1cd0361c492
Montmirail Valentin <valentin_montmirail@ens.univ-artois.fr>
2016-01-21 00:31:53 +0100

    polyTechnique, mising T

---
+++ target/sitePerso/resources/js/animMeter.js
@@ -0,0 +1,17 @@
+$(document).ready( function() {
+
+function animMeter() {
+    $(".meter > span").each(function() {
+                $(this)
+                    .data("origWidth", $(this).width())
+                    .width(0)
+                    .animate({
+                        width: $(this).data("origWidth")
+                    }, 1800);
+            });
+}
+
+animMeter();
+
+});
+
